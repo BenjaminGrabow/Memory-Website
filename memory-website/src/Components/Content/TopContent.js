9a76435a064df3a9f006b00f94f893ae458cba01
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-12 00:21:36 +0200

    Feature: implemented content to TopContent Component

--- memory-website/src/Components/Content/TopContent.js
+++ memory-website/src/Components/Content/TopContent.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const TopContent = () => {
-        return (  );
+        return (
+                <div>
+                        <h1>One platform.
+<br />
+                                From purchase request to general ledger</h1>
+                        <p>Scalable workflows to request, pay, and sync transactions to GL in real-time</p>
+                        <div className="row">
+                                <div>
+                                        <img></img>
+                                        <h3>Pre-approvals</h3>
+                                        <p>Get ahead of spend with a <br />
+                                                streamlined approval process.</p>
+                                        <a href="/"> Learn more -></a>
+                                </div>
+                                <div>
+                                        <img>
+                                        </img>
+                                        <h3>Pre-approvals</h3>
+                                        <p>Get ahead of spend with a <br />
+                                                streamlined approval process.</p>
+                                        <a href="/"> Learn more -></a>
+                                </div>
+                                <div>
+                                        <img></img>
+                                        <h3>Pre-approvals</h3>
+                                        <p>Get ahead of spend with a <br />
+                                                streamlined approval process.</p>
+                                        <a href="/"> Learn more -></a>
+                                </div>
+                                <div>
+                                        <img></img>
+                                        <h3>Pre-approvals</h3>
+                                        <p>Get ahead of spend with a <br />
+                                                streamlined approval process.</p>
+                                        <a href="/"> Learn more -></a>
+                                </div>
+                        </div>
+                </div>);
 }
- 
+
 export default TopContent;
