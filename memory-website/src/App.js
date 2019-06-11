d21240f33634b94e08dc1226376f441780cc82b0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-11 22:50:04 +0200

    Feature: implemented SocialMedia component in App Component

--- memory-website/src/App.js
+++ memory-website/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import SocialMedia from './Components/Footer/SocialMedia';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
   <Header></Header>
+  <SocialMedia/>
     </div>
   );
 }
