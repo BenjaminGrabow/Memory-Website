966ec7177be37f55a8ed2282f2f66b5f2e1fed17
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-11 23:55:06 +0200

    Feature: added more styling to the Icon Component

--- memory-website/src/Components/Icons/SocialMedia.js
+++ memory-website/src/Components/Icons/SocialMedia.js
@@ -3,13 +3,15 @@ import './SocialMedia.css';
 
 const SocialMedia = () => {
         return ( 
+          <div className="icons">
       <div id="wrapper">
         <div className="box" id="facebook">&#62220;</div>
         <div className="box" id="twitter">&#62217;</div>
         <div className="box" id="google">&#62223;</div>
         <div className="box" id="linkedin">&#62232;</div>
       </div>
-         );
+      </div>
+      );
 }
  
 export default SocialMedia;
