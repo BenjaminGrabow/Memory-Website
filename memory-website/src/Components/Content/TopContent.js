247d13460c9b0f5e991764be5fe5b3da8617881a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-12 22:27:04 +0200

    added content, 1 picture and styled component

--- memory-website/src/Components/Content/TopContent.js
+++ memory-website/src/Components/Content/TopContent.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import apple from './apple.jpg';
+import google from './Google.jpg';
+import microsoft from './Microsoft.png';
+import huawei from './Huawei.jpg';
+import siemens from './siemens.jpg';
+import bulb from './Home.jpg'
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
+display: flex;
 
+.left {
+        margin: 1rem;
+}
+.row {
+ img {
+         width: 30%;
+         height: 3rem;
+ }
+}
+
+.right {
+        img {
+                width: 70%;
+                height: 90%;
+                margin: 2rem;
+        }
+}
 `;
 
 const TopContent = () => {
         return ( 
                 <StyledDiv>
-<div className="column">
+<div className="left">
         <h1>The new standard in spend <br/> management</h1>
         <p>Manage yoour entire spend process in one place. Request, pay, and automate <br/>
         your AP accounting with Airbase</p>
         <button>Get a demo</button>
         <p>Trusted by finance teams at all stages</p>
-        <img src={apple} />
-
+        <div className="row">
+                <img src={google} />
+        <img src={microsoft} />
+        </div>
+        <div className="row">
+        <img src={siemens} />
+        <img src={huawei} />
+        </div>
+</div>
+<div className="right">
+        <img src={bulb}/>
 </div>
                 </StyledDiv>
          );
