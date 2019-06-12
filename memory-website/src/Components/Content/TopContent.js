7c2629d6314eb5d998409a708ea450c192b47f4b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-12 21:58:05 +0200

    Feature: New TopContent Component

---
+++ memory-website/src/Components/Content/TopContent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+
+`;
+
+const TopContent = () => {
+        return ( 
+                <StyledDiv>
+<div className="column">
+        <h1>The new standard in spend <br/> management</h1>
+        <p>Manage yoour entire spend process in one place. Request, pay, and automate <br/>
+        your AP accounting with Airbase</p>
+</div>
+                </StyledDiv>
+         );
+}
+ 
+export default TopContent;
