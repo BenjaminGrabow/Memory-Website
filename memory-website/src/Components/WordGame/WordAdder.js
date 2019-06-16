02acd326c25f82d1d258c6539b89239729b5ad81
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-16 12:31:02 +0200

    Feature: styled play again button

--- memory-website/src/Components/WordGame/WordAdder.js
+++ memory-website/src/Components/WordGame/WordAdder.js
@@ -7,6 +7,12 @@ background: #bdc3c7;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #2c3e50, #bdc3c7);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #2c3e50, #bdc3c7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
+.play-again {
+width: 30%;
+margin: 0 auto;
+height: 8rem;
+}
+
 button {
 background-color: red;
 border-radius: 50%;
@@ -176,7 +182,7 @@ class WordAdder extends React.Component {
                         <StyledDiv>
                                 <div className={this.state.showResult === 'off' ? 'off' : 'on'}>
                                         <h1 className="result">{this.state.resultMessage}</h1>
-                                        <button onClick={this.playAgain}>Play Again</button>
+                                        <button className="play-again" onClick={this.playAgain}>Play Again</button>
 
                                 </div>
                                 <div className={this.state.startScreen === 'off' ? 'off' : 'on'} >
