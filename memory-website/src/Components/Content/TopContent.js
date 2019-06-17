07d50d330913dbc8458b7545006200dd19241e8e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-17 18:30:19 +0200

    Feature: added type-writer effect to componenDidMount

--- memory-website/src/Components/Content/TopContent.js
+++ memory-website/src/Components/Content/TopContent.js
@@ -62,21 +62,37 @@ button {
 }
 `;
 
-const text = ['websites', 'illustration', 'hello'];
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-
- function type(){
-
- }
-
 class TopContent extends React.Component {
         constructor(props) {
                 super(props);
                 this.state = {  }
         }
+
+        componentDidMount = () => {
+                const texts = ['websites', 'illustration', 'hello'];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+ (function type(){
+
+        if(count === texts.length){
+                count = 0;
+        }
+
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.querySelector('.typing').textContent = letter;
+        if(letter.length === currentText.length){
+                count++;
+                index = 0;
+        }
+        setTimeout(type, 400);
+}());
+        }
+
         render() { 
                 return ( 
                         <StyledDiv>
